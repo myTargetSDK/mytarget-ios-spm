e5bedb04547ed39a521a9b1649401bc0c14541cb
uranus <uranus@vk.team>
2025-10-29 12:07:53 +0300

    Updated 5.37.1 version

--- Binary/MyTargetSDK.xcframework/ios-arm64/MyTargetSDK.framework/Headers/MTRGNativeAdVideoPlayer.h
+++ Binary/MyTargetSDK.xcframework/ios-arm64/MyTargetSDK.framework/Headers/MTRGNativeAdVideoPlayer.h
@@ -1,6 +1,6 @@
 //
 //  MTRGNativeAdVideoPlayer.h
-//  myTargetSDK 5.37.0
+//  myTargetSDK 5.37.1
 //
 //  Created by Sharin Anton on 17.03.2025.
 //  Copyright © 2025 VK. All rights reserved.
--- Binary/MyTargetSDK.xcframework/ios-arm64_x86_64-simulator/MyTargetSDK.framework/Headers/MTRGNativeBannerAdView.h
+++ Binary/MyTargetSDK.xcframework/ios-arm64_x86_64-simulator/MyTargetSDK.framework/Headers/MTRGNativeBannerAdView.h
@@ -1,6 +1,6 @@
 //
 //  MTRGNativeBannerAdView.h
-//  myTargetSDK 5.37.0
+//  myTargetSDK 5.37.1
 //
 //  Created by Anton Bulankin on 05.12.14.
 //  Copyright (c) 2014 Mail.ru Group. All rights reserved.
